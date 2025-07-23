029ecc6c6a270233176953e368fca9572746ca5c
ducmanh04 <131143136+ducmanh04@users.noreply.github.com>
2025-07-23 11:29:44 +0700

    Add files via upload

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <h1>404 - Không tìm thấy trang</h1>
+    <p>Vui lòng kiểm tra lại đường dẫn.</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+}
+</script>
