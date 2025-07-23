d24c5a2b35f5afbe1fc4e80a0cf99bf3b1134263
ducmanh04 <131143136+ducmanh04@users.noreply.github.com>
2025-07-23 13:28:20 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,27 @@
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../pages/Home.vue'
+import NotFound from '../pages/NotFound.vue'
+import SubmitResponse from '../pages/SubmitResponse.vue'
+import AdminPanel from '../pages/AdminPanel.vue'
+import NotificationCenter from '../pages/NotificationCenter.vue'
+import SubmitApplication from '@/pages/SubmitApplication.vue'
 
-// Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
-import { routes } from 'vue-router/auto-routes'
+const routes = [
+  {
+    path: '/', name: 'Home', component: HomePage
+  },
+  {
+    path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound
+  },
+  { path: '/phan-hoi', name: 'PhanHoi', component: SubmitResponse },
+  { path: '/quan-tri', name: 'QuanTri', component: AdminPanel },
+  { path: '/thong-bao', name: 'ThongBao', component: NotificationCenter },
+  { path: '/doanh-nghiep', name: 'DoanhNghiep', component: SubmitApplication }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes),
-})
-
-// Workaround for https://github.com/vitejs/vite/issues/11804
-router.onError((err, to) => {
-  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-    if (localStorage.getItem('vuetify:dynamic-reload')) {
-      console.error('Dynamic import error, reloading page did not fix it', err)
-    } else {
-      console.log('Reloading page to fix dynamic import error')
-      localStorage.setItem('vuetify:dynamic-reload', 'true')
-      location.assign(to.fullPath)
-    }
-  } else {
-    console.error(err)
-  }
-})
-
-router.isReady().then(() => {
-  localStorage.removeItem('vuetify:dynamic-reload')
+  history: createWebHistory(),
+  routes
 })
 
 export default router
