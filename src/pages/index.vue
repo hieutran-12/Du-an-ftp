0e6e7dc95f205261e9646d08a0d88442abc35a76
ducmanh04 <131143136+ducmanh04@users.noreply.github.com>
2025-07-23 16:12:09 +0700

    Create index.vue

---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Status />
+</template>
+
+<script setup>
+//import { useRouter } from "vue-router";
+import { onMounted } from "vue";
+import SubmitApplication from "./SubmitApplication.vue";
+import Status from "./Status.vue";
+const router = useRouter();
+
+onMounted(() => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    router.replace("/Login"); // chỉ chuyển nếu chưa login
+  }
+});
+</script>
+<style scoped></style>
