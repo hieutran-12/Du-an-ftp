33b11043816f836c59e8d2f7f59fe62fc28f20cd
ducmanh04 <131143136+ducmanh04@users.noreply.github.com>
2025-07-23 09:45:13 +0700

    Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,18 +14,30 @@
             <p class="highlight-text">Tổng doanh nghiệp thành lập mới: 15.228</p>
             <p class="highlight-text">Doanh nghiệp quay trở lại hoạt động: 9.104</p>
             <p class="highlight-text">Doanh nghiệp giải thể: 1.750</p>
-            <p class="highlight-text">Tạm ngưng hoạt động</p>
+            <p class="highlight-text">Doanh nghiệp tạm ngưng hoạt động: 5.924</p>
           </div>
       </v-col>
     </v-row>
-    <div style="display: grid;">
+    <div style="display: flex; gap: 16px;">
         <div class="title">
-          <img src="https://quanlytin.emdep.vn/Share/Image/2023/11/29/dau-cham-than-co-y-nghia-gi-113039567.jpg">
+          <img src="https://media.istockphoto.com/id/962564820/vi/vec-to/d%E1%BA%A5u-ch%E1%BA%A5m-than-trong-bi%E1%BB%83u-t%C6%B0%E1%BB%A3ng-vector-gi%E1%BB%8Dng-n%C3%B3i-bong-b%C3%B3ng-kh%C3%A1i-ni%E1%BB%87m-os-ch%C3%BA-%C3%BD-ho%E1%BA%B7c-d%E1%BA%A5u-hi%E1%BB%87u.jpg?s=612x612&w=0&k=20&c=Xmht7NABumBXkAbk44x7JemX71QMIgctR0hWVJYFML0=">
           <p>Dịch vụ thông tin</p>
         </div>
         <div class="title">
-          <img src="https://quanlytin.emdep.vn/Share/Image/2023/11/29/dau-cham-than-co-y-nghia-gi-113039567.jpg">
-          <p>Dịch vụ thông tin</p>
+          <img src="https://clc.hust.edu.vn/xmedia/2021/03/form-dang-ky-aptis-minh-hoa.png">
+          <p>Đăng ký doanh nghiệp</p>
+        </div>
+        <div class="title">
+          <img src="https://png.pngtree.com/png-vector/20190629/ourlarge/pngtree-reports-icon-for-your-project-png-image_1521526.jpg">
+          <p>Báo cáo điện tử</p>
+        </div>
+        <div class="title">
+          <img src="https://i-law.vn/uploads/articles/68399/ho_kinh_doanh_la_gi_2702164641.png">
+          <p>Đăng ký hộ kinh doanh</p>
+        </div>
+        <div class="title">
+          <img src="https://img.pikbest.com/png-images/qiantu/business-handshake-icon_2678293.png!sw800">
+          <p>Đăng ký hợp tác xã</p>
         </div>
     </div>
   </v-container>
@@ -38,7 +50,7 @@ export default {
 </script>
 <style>
 .highlight-text {
-  background-color: #1b8181;
+  background-color: #1cd4d4;
   padding: 12px 16px;
   border-radius: 8px;
   color: #ffffff;
@@ -46,14 +58,20 @@ export default {
   margin-bottom: 5px;
 }
 .title{
-  background-color: aqua;
+  text-align: center;
+  background-color: gray;
+  padding: 12px;
+  border-radius: 10px;
+  width: 230px;
 }
 img{
   width: 200px;
   height: 200px;
+  border-radius: 10px;
 }
 p{
-  text-align: center;
-  color: #ffffff;
+  color: white;
+  padding: 6px;
+  margin-top: 6px;
 }
 </style>
